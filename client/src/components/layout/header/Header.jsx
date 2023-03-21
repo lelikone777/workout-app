2552663d74e8c01130f5719fa527bacd0276b00b
lelikone777 <2257855@gmail.com>
2023-03-21 17:42:25 +0300

    13

--- client/src/components/layout/header/Header.jsx
+++ client/src/components/layout/header/Header.jsx
@@ -8,7 +8,7 @@ import Hamburger from '../hamburger/Hamburger'
 
 import styles from './Header.module.scss'
 
-const Header = ({ backLink = '' }) => {
+const Header = ({ backLink = '/' }) => {
 	const { pathname } = useLocation()
 	const navigate = useNavigate()
 
@@ -16,24 +16,28 @@ const Header = ({ backLink = '' }) => {
 
 	return (
 		<header className={styles.header}>
-			{pathname !== '/' ? (
-				<button
-					onClick={() => {
-						navigate(backLink)
-					}}
-				>
-					<IoMdArrowBack fill='#fff' fontSize={29} />
-				</button>
-			) : (
-				<button
-					onClick={() => {
-						navigate(isAuth ? '/profile' : '/auth')
-					}}
-				>
-					<SlUser fill='#fff' fontSize={25} />
-				</button>
+			{isAuth && (
+				<>
+					{pathname === '/' && isAuth ? (
+						<button
+							onClick={() => {
+								navigate('/profile')
+							}}
+						>
+							<SlUser fill='#fff' fontSize={25} />
+						</button>
+					) : (
+						<button
+							onClick={() => {
+								navigate(isAuth ? backLink : '/auth')
+							}}
+						>
+							<IoMdArrowBack fill='#fff' fontSize={29} />
+						</button>
+					)}
+					<Hamburger />
+				</>
 			)}
-			<Hamburger />
 		</header>
 	)
 }
